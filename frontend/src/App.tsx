9052f125f52d48322d116c3d525b129c81c7f5f8
Danila Pisarev <danila.pisarev@moex.com>
2026-07-06 17:34:17 +0300

    add backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,30 @@
-import { HELLO_TEXT } from "./config";
+import { useState } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
+  const [data, setData] = useState<string>();
+
   return (
     <div
       style={{
+        flexDirection: "column",
         display: "flex",
         justifyContent: "center",
         height: "100vh",
         alignItems: "center",
       }}
     >
-      {HELLO_TEXT}
+      <button
+        onClick={async () => {
+          const response = await (await fetch(`${API_URL}/hello`)).text();
+          setData(response);
+        }}
+      >
+        Fetch
+      </button>
+
+      {data}
     </div>
   );
 }
